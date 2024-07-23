779848f728210988a5f3f51879b7bf4ea1b00c9e
AshimsGithub <thapaas@shernet.sheridancollege.ca>
2024-07-22 23:41:02 -0400

    I did it.

---
+++ src/ca/sheridancollege/project/HumanPlayer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+public class HumanPlayer extends Player {
+    public HumanPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+        // Logic for human player to play a card
+    }
+}
+
