99b50307a93ee00fd054e8c70f00bb428171190b
AshimsGithub <thapaas@shernet.sheridancollege.ca>
2024-07-22 23:41:27 -0400

    I did it.

---
+++ src/ca/sheridancollege/project/ComputerPlayer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+public class ComputerPlayer extends Player {
+    public ComputerPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+        // Logic for computer player to play a card
+    }
+}
+
