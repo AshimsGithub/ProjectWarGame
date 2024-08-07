d444cb7c7347fc4abb06d84dca85761f43572439
AshimsGithub <thapaas@shernet.sheridancollege.ca>
2024-08-07 15:45:16 -0400

    Ashim Thapa 3:45 pm

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -10,39 +10,25 @@ package ca.sheridancollege.project;
  * enough to be instantiated for any Card game. Students wishing to add to the code 
  * should remember to add themselves as a modifier.
  * @author dancye, 2018
- * @modifier Jonathan Penava, 2024
+ * @modifier Ashim Thapa, 2024
  */
 public class Card {
-    private final String suit; // The suit of the card (e.g., Hearts, Diamonds)
-    private final String rank; // The rank of the card (e.g., 2, 3, Jack, Queen)
+    private String suit;
+    private String rank;
 
-    /**
-     * Constructor to create a card with a specific suit and rank.
-     * @param suit the suit of the card
-     * @param rank the rank of the card
-     */
     public Card(String suit, String rank) {
         this.suit = suit;
         this.rank = rank;
     }
 
-    /**
-     * @return the suit of the card
-     */
     public String getSuit() {
         return suit;
     }
 
-    /**
-     * @return the rank of the card
-     */
     public String getRank() {
         return rank;
     }
 
-    /**
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
     @Override
     public String toString() {
         return rank + " of " + suit;
